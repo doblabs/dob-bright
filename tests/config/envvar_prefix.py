b5f5ec4e71fc23d9513b5a4dab91549e3a6ada04
landonb <landonb@users.noreply.github.com>
2020-12-22 04:13:44 -0600

    Tests: Init. resources that dob would otherwise init.

--- tests/config/envvar_prefix.py
+++ tests/config/envvar_prefix.py
@@ -2,7 +2,7 @@
 #
 #   https://github.com/tallybark/dob-bright
 #
-# Copyright © 2020 Landon Bouma. All rights reserved.
+# Copyright © 2019-2020 Landon Bouma. All rights reserved.
 #
 # This program is free software:  you can redistribute it  and/or  modify it under the
 # terms of the GNU General Public License as published by the Free Software Foundation,
@@ -15,22 +15,9 @@
 # If you lost the GNU General Public License that ships with this software
 # repository (read the 'LICENSE' file), see <http://www.gnu.org/licenses/>.
 
-""""""
+from config_decorator import KeyChainedValue
 
-# For the fixture to work, either import what you need, or glob it all.
-# - Globbing is easier and more akin to how conftest.py normally magically works.
-# - Skip the linter's two gripes:
-#    noqa: F401 'foo.bar' imported but unused
-#    noqa: F403 'from foo.bar import *' used; unable to detect undefined names
-from dob_bright.tests.conftest import *  # noqa: F401, F403
+from dob_bright import __package_name__
 
-pytest_plugins = (
-    # Import tmp_appdirs fixture.
-    'easy_as_pypi_apppth.test_mock',
-
-    # Set KeyChainedValue._envvar_prefix.
-    'tests.config.envvar_prefix',
-    # Call app_dirs.register_application.
-    'tests.config.init_app_dirs',
-)
+KeyChainedValue._envvar_prefix = '{}_'.format(__package_name__.upper())
 
